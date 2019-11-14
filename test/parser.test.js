461ec28a5c69edf8b2daecbe9464fb537b160b90
Goran <cabarkapa.goran@gmail.com>
2019-11-14 13:32:48 +0100

    Resolved the module exports error in parser.js, all tests passing

--- test/parser.test.js
+++ test/parser.test.js
@@ -14,16 +14,6 @@ describe('parser.js tests', () => {
             const result = parser.listingsScraper(html);
             expect(result.length).to.equal(35);
         });
-        /* test iznad daje: ReferenceError: scraper is not defined
-                            at Node.$.map (lib/parser.js:22:60)
-                            at /home/goran/workspace/zzzcg/node_modules/cheerio/lib/api/traversing.js:306:18
-                            at /home/goran/workspace/zzzcg/node_modules/lodash/_baseReduce.js:18:9
-                            at /home/goran/workspace/zzzcg/node_modules/lodash/_createBaseEach.js:24:11
-                            at baseReduce (node_modules/lodash/_baseReduce.js:15:3)
-                            at Object.reduce (node_modules/lodash/reduce.js:48:10)
-                            at initialize.exports.map (node_modules/cheerio/lib/api/traversing.js:305:23)
-                            at Object.listingsScraper (lib/parser.js:22:36)
-                            at Context.it (test/parser.test.js:14:35)*/
     });
     describe('scraper() Test', () => {
         it('name should equal "NASTAVNIK/CA FIZIKE"', () => {
