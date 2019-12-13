de06215e8b8a366e0bfb19ff7eecf5d334bf0020
Goran <cabarkapa.goran@gmail.com>
2019-12-13 16:17:47 +0100

    Modified sls.yml for stream event

--- streamFunction.js
+++ streamFunction.js
@@ -1,4 +1,4 @@
-module.exports.streamReader =  async function(event, context) {
+module.exports.handler =  async function(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return context.logStreamName
   }
