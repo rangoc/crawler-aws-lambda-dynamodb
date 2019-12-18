d7d861f6b6f68dd69aa9c57211d00a903d91f579
Goran <cabarkapa.goran@gmail.com>
2019-12-18 15:08:03 +0100

    Modified graphqlClient definition

--- streamFunction.js
+++ streamFunction.js
@@ -1,7 +1,7 @@
-const gql  = require('graphql-tag')
-const AWSAppSyncClient = require('aws-appsync')
+const gql  = require('graphql-tag');
+const AWSAppSyncClient = require('aws-appsync');
 
-const graphqlClient = new AWSAppSyncClient({
+const graphqlClient = AWSAppSyncClient({
   url: process.env.url,
   region: process.env.region,
   auth: {
