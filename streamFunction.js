1cee6272b8a74d897b9c901ce417175fa8fef3ea
Goran <cabarkapa.goran@gmail.com>
2019-12-18 12:06:44 +0100

    Modified streamFunction.js for mutations on graphql api

--- streamFunction.js
+++ streamFunction.js
@@ -1,4 +1,35 @@
+import { gql } from 'graphql-tag';
+
 module.exports.handler =  async function(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
+    
+    const {employer, location, datePosted, title, url } = event;
+    
+    
+    const mutation = gql`
+    mutation createJob {
+      createJob(input: { 
+        
+        employer: "${employer}",
+        location: "${location}",
+        datePosted: "${datePosted}",
+        title: "${title}",
+        url: "${url}"
+      } 
+      }) {
+          id
+          employer
+          location
+          datePosted
+          title
+          url
+      }
+    }
+  `;
+
+  await graphqlClient.mutate({ mutation });
+  return {
+    statusCode: 200,
+    body: 'Mutation success',
   }
+}
