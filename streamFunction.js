afa8a211c4132b153e909cfa7d6a1b74bbc0d7cc
Goran <cabarkapa.goran@gmail.com>
2019-12-23 11:44:04 +0100

    Trying to configure streamFunction to send mutations to API

--- streamFunction.js
+++ streamFunction.js
@@ -3,20 +3,41 @@ const AWSAppSyncClient = require('aws-appsync').default;
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-const client = new AWSAppSyncClient({
-  url: process.env.url,
-  region: process.env.region,
-  auth: {
-    type: process.env.authType,
-    apiKey: process.env.apiKey
-  },
-  disableOffline: true
-});
+
 module.exports.handler =  async function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    const {employer, location, datePosted, title, url } = event;
-  
-    const mutation = gql`
+    //const {employer, location, datePosted, title, url } = event;
+    const client = new AWSAppSyncClient({
+      url: process.env.url,
+      region: process.env.region,
+      auth: {
+        type: process.env.authType,
+        apiKey: process.env.apiKey
+      },
+      disableOffline: true
+    });
+    const query = gql`
+      query AllJobs {
+        allJobs {
+            id
+            employer
+            location
+            datePosted
+            title
+            url
+            
+        }
+      }
+    `;
+    client.hydrated().then(function (client) {
+      //Now run a query
+      client.query({ query: query });
+      client.query({ query: query, fetchPolicy: 'network-only' })   //Uncomment for AWS Lambda
+          .then(function logData(data) {
+              console.log('results of query: ', data);
+          })
+          .catch(console.error);
+    });
+    /*const mutation = gql`
     mutation createJob {
       createJob(input: { 
         
@@ -39,5 +60,5 @@ await client.mutate({ mutation });
   return {
     statusCode: 200,
     body: 'Mutation success',
-  }
+  }*/
 }
